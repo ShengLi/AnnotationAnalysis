22cf882cd9d3dc4347f390b45f0f0a6a2bf22432
ShengLi <shelly1436@gmail.com>
2011-12-15 00:13:41 -0500

    add peall annotation and gene count code

---
+++ R/peall_aligns.R
@@ -0,0 +1,40 @@
+#========== PEALL aligns from bam ===========
+
+# load library
+library(GenomicFeatures)
+library(GenomicRanges)
+library(Rsamtools)
+library(stringr)
+
+# load annotation reference
+load("/scratchLocal01/shl2018/eRRBS/bcdata/myCpG/hg18ref.Rdata")
+
+# function
+
+# load alignment from bam file and get read count and annotation for each gene.
+bamToCount=function(bamFile,anno=hg18ref.exon){
+# from bam to genomicRanges object
+	aligns = readBamGappedAlignments(bamFile)
+	
+# get count
+	counts <- countOverlaps(anno, aligns)
+	
+# get rpkm
+	numBases <- sum(width(anno))
+	geneLengthsInKB <- numBases/1000
+	millionsMapped <- sum(counts)/1e+06
+	rpm <- counts/millionsMapped
+	rpkm <- rpm/geneLengthsInKB
+	
+	return(list(counts=counts,rpkm=rpkm, bamFile=bamFile))
+	
+}
+
+# end of function
+
+# read file
+bamDir = '/scratchLocal01/shl2018/peall/data/'
+bamFiles = str_c(bamDir,'s_',1:8,'_hg18.bam')
+
+
+allCounts = lapply(bamFiles, bamToCount)
